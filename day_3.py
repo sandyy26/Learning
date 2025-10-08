ab5602c270832caa3e076cef0cbff6354a94d15b
sandyy26 <sandhiyabalan26@gmail.com>
2025-10-08 23:22:15 +0530

    day_3.py

---
+++ day_3.py
@@ -0,0 +1,35 @@
+#arithmetic operators
+'''
+
+a=100
+b=567
+c=875
+print("addition:",a+b+c)
+print("subtract:",a-b-c)
+print("multiply:",a*b*c)
+print("division:",a/b/c)
+print("floor division:",a//b//c)
+print("exponentiation:",a**b**c)
+
+'''
+#conditional operators
+'''
+
+a=90
+b=34
+print(a<b)
+print(a>b)
+print(a==b)
+print(a<=b)
+print(a>=b)
+
+'''
+#logical operators
+'''
+a=False
+b=True
+print(a and b)
+print(a or b)
+print(not b)
+
+'''
