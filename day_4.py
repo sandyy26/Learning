54e44a2477602ea2f1ed3d9e952bdc0dd30b6471
sandyy26 <sandhiyabalan26@gmail.com>
2025-10-10 22:57:37 +0530

    day_4

---
+++ day_4.py
@@ -0,0 +1,38 @@
+#case sensitive variable names
+'''
+
+a=7
+A='sandy'
+print(a,A)
+
+'''
+#unpacking
+'''
+
+games=['cricket','football','kabaddi']
+a,b,c=games
+print(a,b,c)
+print(a + b + c)
+print(a)
+print(b)
+print(c)
+
+'''
+#note:look at the space between single quote for the word'he'
+'''
+
+x='he '
+y='is'
+z='okey'
+print(x+y+z)
+
+'''
+#create a variable inside a function,with the same name as the global variable
+
+
+x="wow"
+def myfunc():
+    x='super'
+    print(x)
+myfunc()
+print(x)
